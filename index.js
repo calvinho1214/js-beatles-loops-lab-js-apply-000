3cbd78d575233e4d719d4c93d81b916414c6b8d2
calvinho1214 <calvinho1214@gmail.com>
2018-08-23 16:34:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var emptyArr = [];
+  for (var i = 0; i < musicians.length; i++){
+    var muses = musicians[i];
+    var instru = instruments[i];
+    emptyArr.push(muses + " plays " + instru);
+  }
+  return emptyArr;
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  var emptArr = [];
+  while(i < facts.length){
+    emptArr.push(facts[i] + "!!!");
+    i++;
+  }
+  return emptArr;
+}
+
+function iLoveTheBeatles(numb){
+  var emptArr=[];
+  
+}
